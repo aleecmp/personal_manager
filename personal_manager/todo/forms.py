3544404abe3899c02eea29c858c8201ffca97c51
aleecmp <ale.cmpb@gmail.com>
2023-01-18 14:51:14 -0300

    Fix: add fix typo

--- personal_manager/todo/forms.py
+++ personal_manager/todo/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
-form .models import Todo
+from .models import Todo
 
-class TodoForm(ModelForm)
+class TodoForm(ModelForm):
     class Meta:
         model = Todo
         fields = '__all__'
