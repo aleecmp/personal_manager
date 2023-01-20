7ea0f7cef8b34f9c943aa3ffaba4d97dab8f203f
aleecmp <ale.cmpb@gmail.com>
2023-01-20 16:05:51 -0300

    feat: add feature contact

--- personal_manager/contact/views.py
+++ personal_manager/contact/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Contact
 
-# Create your views here.
+def index(request):
+    search = request.GET.get('search', '')
+    contacts = Contact.objects.filter(
+            name__icontains=search)
+    context = {
+            'contacts': contacts
+            }
+    return render(request, 'contact/index.html', context)
