f2e49daf0df6fddfba258f689d95b7df84f59697
aleecmp <ale.cmpb@gmail.com>
2023-01-27 11:53:28 -0300

    fix: add fix to todo edit, it is not possible to add a date in the past

--- personal_manager/todo/forms.py
+++ personal_manager/todo/forms.py
@@ -1,8 +1,14 @@
-from django.forms import ModelForm
+from django.forms import DateInput, ModelForm
 from .models import Todo
+import datetime
 
 
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
         exclude = ("date",)
+        widgets = {
+            "estimated_end": DateInput(
+                attrs={"type": "date", "min": datetime.date.today().isoformat()}
+            ),
+        }
