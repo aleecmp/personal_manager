6784c1e085df905fce1f3e0493b8910cffe10016
aleecmp <ale.cmpb@gmail.com>
2023-02-01 22:19:37 -0300

    add config

--- personal_manager/settings.py
+++ personal_manager/settings.py
@@ -130,13 +130,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
+    os.path.join(BASE_DIR, "static"),
 ]
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
 
 
 # Default primary key field type
